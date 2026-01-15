13a421c636aff005ce97435ea82b4582de725cbb
manuny_a <manyadance.ru@gmail.com>
2026-01-15 12:39:21 +1000

    Initial Commit

---
+++ CarShop/CarShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarShopApp.swift
+//  CarShop
+//
+//  Created by Mary Lenina on 15.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CarShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
